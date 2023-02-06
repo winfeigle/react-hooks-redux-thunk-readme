02d1b02499524d54235c10620421a9bc7fc495b3
Win Feigle <win.feigle@gmail.com>
2023-02-05 17:10:36 -0700

    commit tree 6e77674aa400bbd343945a7f6920325429f2973e

--- src/features/astronauts/astronautsSlice.js
+++ src/features/astronauts/astronautsSlice.js
@@ -1,14 +1,26 @@
 // Action Creators
 export function fetchAstronauts(astronauts) {
-  return {
-    type: "astronauts/astronautsLoaded",
-    payload: astronauts,
+  return function (dispatch) {
+    // dispatch an initial action to set up a "loading" state
+    dispatch({ type: "astronauts/astronautsLoading" });
+
+    // initiate a network request with fetch
+    fetch("http://api.open-notify.org/astros.json")
+      .then((response) => response.json())
+      .then((astronauts) =>
+        // when we have data from the response, dispatch another action to add the data to our Redux store
+        dispatch({
+          type: "astronauts/astronautsLoaded",
+          payload: astronauts.people,
+        })
+      );
   };
 }
 
 // Reducers
 const initialState = {
   entities: [], //array of astronauts
+  status: "idle", // loading status for fetch
 };
 
 export default function reducer(state = initialState, action) {
@@ -16,7 +28,14 @@ export default function reducer(state = initialState, action) {
     case "astronauts/astronautsLoaded":
       return {
         ...state,
+        status: "idle",
         entities: action.payload,
+      };
+
+    case "astronauts/astronautsLoading":
+      return {
+        ...state,
+        status: "loading",
       };
 
     default:
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+// Added applyMiddleware
+import { createStore, applyMiddleware } from "redux";
 import App from "./App";
 import rootReducer from "./reducer";
+// Adding redux-thunk middleware
+import ThunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composedEnhancer = composeWithDevTools(applyMiddleware(ThunkMiddleware));
+
+const store = createStore(rootReducer, composedEnhancer);
+
 
 ReactDOM.render(
   <Provider store={store}>
